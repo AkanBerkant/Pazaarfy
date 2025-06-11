3d6e40aff841571d8acbbed2689d8300d7e90135
AkanBerkant <64760391+AkanBerkant@users.noreply.github.com>
2025-06-11 14:50:23 +0300

    fix

--- android/app/src/main/java/com/pazaarfy/MainActivity.kt
+++ android/app/src/main/java/com/pazaarfy/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.pazaarfy
+package com.pazaar
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/pazaarfy/MainApplication.kt
+++ android/app/src/main/java/com/pazaarfy/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.pazaarfy
+package com.pazaar
 
 import android.app.Application
 import android.content.res.Configuration
